65a72ec5019bebcac7d7a46abbd6be242dfc826a
Kyler Yohn <kyyohn@gmail.com>
2019-04-28 16:42:53 -0400

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 //create a connection with mysql
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "Kyleryohn12!",
     database: "burger_db"
 });
+
+}
 
 //make the connection with mysql
 connection.connect(function(err){
